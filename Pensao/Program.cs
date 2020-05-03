d1a5c552dfe2d9ecc091f1e127bb02aac5f7e696
freitasJV <jose.freitas16@fatec.sp.gov.br>
2020-05-03 16:22:53 -0300

    Adicionando tratamento de excessões

--- Pensao/Program.cs
+++ Pensao/Program.cs
@@ -14,64 +14,75 @@ namespace Pensao
 
         static void Main(string[] args)
         {
-            int qtdeQuartos = 10, qtdeAlugados = 0;
-            bool fim = false;
-
-            Locatario[] quartos = new Locatario[qtdeQuartos];
-
-            while (qtdeQuartos - qtdeAlugados > 0 && !fim)
+            try
             {
-                Console.WriteLine($"Quantos quartos deseja alugar? no momento temos {qtdeQuartos - qtdeAlugados} vagas");
-                int qtde = int.Parse(Console.ReadLine());
+                int qtdeQuartos = 10, qtdeAlugados = 0;
+                bool fim = false;
 
-                if (qtde > qtdeQuartos - qtdeAlugados || qtde<=0)
-                    Console.WriteLine("Quantidade inválida");
-                else
+                Locatario[] quartos = new Locatario[qtdeQuartos];
+
+                while (qtdeQuartos - qtdeAlugados > 0 && !fim)
                 {
-                    for (int i = 1; i <= qtde; i++)
+                    Console.WriteLine($"Quantos quartos deseja alugar? no momento temos {qtdeQuartos - qtdeAlugados} vagas");
+                    int qtde = int.Parse(Console.ReadLine());
+
+                    if (qtde > qtdeQuartos - qtdeAlugados || qtde <= 0)
+                        Console.WriteLine("Quantidade inválida");
+                    else
                     {
-                        Console.WriteLine($"Digite o nome do {i}º Locatário: ");
-                        string nome = Console.ReadLine();
-                        Console.WriteLine($"Digite o email do {i}º Locatário: ");
-                        string email = Console.ReadLine();
-                        Console.WriteLine($"Digite o quarto escolhido de 0 a {qtdeQuartos-1}: ");
-                        int numero = int.Parse(Console.ReadLine());
-
-                        while (numero < 0 | numero >= qtdeQuartos)
+                        for (int i = 1; i <= qtde; i++)
                         {
-                            Console.WriteLine($"Digite um quarto de 0 a {qtdeQuartos - 1}: ");
-                            numero = int.Parse(Console.ReadLine());
+                            Console.WriteLine($"Digite o nome do {i}º Locatário: ");
+                            string nome = Console.ReadLine();
+                            Console.WriteLine($"Digite o email do {i}º Locatário: ");
+                            string email = Console.ReadLine();
+                            Console.WriteLine($"Digite o quarto escolhido de 0 a {qtdeQuartos - 1}: ");
+                            int numero = int.Parse(Console.ReadLine());
+
+                            while (numero < 0 | numero >= qtdeQuartos)
+                            {
+                                Console.WriteLine($"Digite um quarto de 0 a {qtdeQuartos - 1}: ");
+                                numero = int.Parse(Console.ReadLine());
+                            }
+
+                            while (quartos[numero] != null)
+                            {
+                                Console.WriteLine("Quarto indisponível, escolha outro quarto: ");
+                                numero = int.Parse(Console.ReadLine());
+                            }
+
+                            quartos[numero] = new Locatario
+                            {
+                                Nome = nome,
+                                Email = email
+                            };
                         }
-
-                        while (quartos[numero] != null )
-                        {
-                            Console.WriteLine("Quarto indisponível, escolha outro quarto: ");
-                            numero = int.Parse(Console.ReadLine());
-                        }
-
-                        quartos[numero] = new Locatario
-                        {
-                            Nome = nome,
-                            Email = email
-                        };
+                        qtdeAlugados += qtde;
                     }
-                    qtdeAlugados += qtde;
+
+                    Console.WriteLine("Deseja finalizar o programa? Em caso positivo digite sim e pressione enter, caso contrário pressione enter");
+                    string final = Console.ReadLine().ToLower();
+
+                    if (final.Equals("sim"))
+                        fim = true;
                 }
 
-                Console.WriteLine("Deseja finalizar o programa? Em caso positivo digite sim e pressione enter, caso contrário pressione enter");
-                string final = Console.ReadLine().ToLower();
-
-                if (final.Equals("sim"))
-                    fim = true;
-            }
-
-            Console.WriteLine("Quartos Ocupados: ");
-            for(int i = 0; i< qtdeQuartos; i++)
-            {
-                if(quartos[i] != null)
+                Console.WriteLine("Quartos Ocupados: ");
+                for (int i = 0; i < qtdeQuartos; i++)
                 {
-                    Console.WriteLine($"{i}: {quartos[i]}");
+                    if (quartos[i] != null)
+                    {
+                        Console.WriteLine($"{i}: {quartos[i]}");
+                    }
                 }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine($"Erro de formatação: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Erro inesperado: {e.Message}");
             }
         }
     }
